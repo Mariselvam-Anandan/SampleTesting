b23c6bec7d415f755519b4607155cd0f004ff792
Mariselvam-Anandan <amstester.ams01@gmail.com>
2024-03-12 16:42:40 +0530

    Added successfully

---
+++ Maridemo/src/com/org/demo/Return.java
@@ -0,0 +1,40 @@
+package com.org.demo;
+
+public class Return {
+
+	static String weyou = "89077779797777977779";
+	
+	int ss = 9999;
+	String gh = "sder09";
+	
+	public static void main(String[]args)
+	{
+	Return sss = new Return();
+	System.out.println(sss.gh);
+	System.out.println("Mariselvam");
+	System.out.println("MCA");
+	System.out.println("Test Engineer");
+	
+	Boolean a=true;
+	byte b=30;
+	short c=3902;
+	int d=400023232;
+	long h=900000000L;
+	float j=9000.899f;
+	double k=39902232;
+	char o='s';
+	
+	System.out.println(a);
+	System.out.println(b);
+	System.out.println(c);
+	System.out.println(d);
+	System.out.println(h);
+	System.out.println(j);
+	System.out.println(k);
+	System.out.println(o);
+	System.out.println(weyou);
+	System.out.println(sss.ss);
+	System.out.println(sss.gh);
+	}
+	
+}
