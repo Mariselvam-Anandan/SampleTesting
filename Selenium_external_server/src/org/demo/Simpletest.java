91293e181e058121574bce1282c86d78b51ffb8e
Harish <hk11471297@gmail.com>
2024-03-13 14:57:36 +0530

    Simpletest modified

--- Selenium_external_server/src/org/demo/Simpletest.java
+++ Selenium_external_server/src/org/demo/Simpletest.java
@@ -6,16 +6,17 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class Simpletest {
 	
-	public static void browserSetup() {
+	public static void browserSetup() throws InterruptedException {
 		System.setProperty("webdriver.firefox.driver", "D:\\THI Infotech\\Webdriver\\geckodriver.exe");
 		WebDriver driver = new FirefoxDriver();
 		
 		driver.get("https://www.flipkart.com/");
 		driver.manage().window().maximize();
-		
+		Thread.sleep(3000);
+		driver.quit();
 	}
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		browserSetup();
 	}
 }
